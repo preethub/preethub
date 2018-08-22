60a3358564da515108526a4625fda62e0106ec3d
preethub <42606094+preethub@users.noreply.github.com>
2018-08-22 22:52:15 +0530

    Create users.php

---
+++ ph-admin/functions/users.php
@@ -0,0 +1,25 @@
+<?php 
+
+$users_data = $db->query("SELECT * FROM `". PH_PRIFIX ."users`");   
+
+if(isset($_POST['add_user'])){
+
+$username = $db->escape($_POST['username']);
+
+$password = $db->escape($_POST['password']);
+
+$email = $db->escape($_POST['email']);
+
+// attempt insert query execution
+
+$sql = "INSERT INTO ". PH_PRIFIX ."users (role, email, username, password) VALUES ('Author', '$email', '$username', '$password')";
+
+if($db->query($sql) === TRUE){
+
+$echo = "Records added successfully.";
+
+} else{
+
+$echo = "ERROR: Could not able to execute $sql. ". mysqli_error($db->link);
+}
+}
