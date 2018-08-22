aac7bd568eb1d8ee0d2a73e06f3d6f8617d45873
preethub <42606094+preethub@users.noreply.github.com>
2018-08-22 15:55:07 +0530

    Create header.php

---
+++ ph-preet/view/temp/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php addcss('style.css'); ?>
+</head>
+<body>
+<div class="header">
+  <h2><a href="<?php echo $site_url; ?>" style="color:white"><?php echo $site_name; ?></a></h2>
+  </div>
