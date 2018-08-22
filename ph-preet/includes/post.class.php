9aa235e20118bf50ad6b941d426c11d19eca1e1a
preethub <42606094+preethub@users.noreply.github.com>
2018-08-22 15:34:55 +0530

    Create post.class.php

---
+++ ph-preet/includes/post.class.php
@@ -0,0 +1,37 @@
+<?php
+
+
+Class Post{
+	
+	
+	
+	function getpost($url){
+		
+		global $db;
+		
+		if(!empty($url)){
+			
+			$postid = $url;
+		}else{
+			
+			header('location: index.php');
+		}
+		
+		$query = "SELECT * FROM ". PH_PRIFIX ."posts Where id='$postid'";
+		
+		$posts = $db->select($query);
+		
+		
+		return $posts;
+		
+			}
+	
+	
+	
+	
+	
+
+
+
+
+}
