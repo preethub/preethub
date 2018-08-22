03f32357df043d8d61d76db3a052e972ae22bf16
preethub <42606094+preethub@users.noreply.github.com>
2018-08-22 22:58:44 +0530

    Create sidebar.php

---
+++ ph-admin/includes/sidebar.php
@@ -0,0 +1,27 @@
+  <!-- Sidebar menu-->
+    <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+    <aside class="app-sidebar">
+        </div>
+      </div>
+      <ul class="app-menu">
+        <li><a class="app-menu__item" href="index.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">Users</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li><a class="treeview-item" href="users.php"><i class="icon fa fa-circle-o"></i> All users</a></li>
+            <li><a class="treeview-item" href="add_user.php" target="_blank" rel="noopener"><i class="icon fa fa-circle-o"></i>Add User</a></li>
+                </ul>
+        </li>
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Posts</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li><a class="treeview-item" href="posts.php"><i class="icon fa fa-circle-o"></i> All Posts</a></li>
+            <li><a class="treeview-item" href="addpost.php"><i class="icon fa fa-circle-o"></i> Add Post</a></li>            
+          </ul>
+        </li>
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Settings</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li><a class="treeview-item" href="Settings.php"><i class="icon fa fa-circle-o"></i> Settings</a></li>
+           
+          </ul>
+        </li>
+      </ul>
+    </aside>
