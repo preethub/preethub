430b9f4bf308b3b88530b049cb7b855b5454ef7a
preethub <42606094+preethub@users.noreply.github.com>
2018-08-22 22:29:13 +0530

    Create index.php

---
+++ ph-admin/index.php
@@ -0,0 +1,36 @@
+<?php 
+require('inc.php');
+require('functions/index.php');
+require('includes/header.php');
+?>
+  <main class="app-content">
+      <div class="app-title">
+      <h1><i class="fa fa-dashboard"></i> Dashboard</h1>
+        <ul class="app-breadcrumb breadcrumb">
+          <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+          <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+        </ul>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-lg-3">
+          <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
+            <div class="info">
+              <h4>Users</h4>
+              <p><b><?php echo $total_users; ?></b></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+          <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
+            <div class="info">
+              <h4>Posts</h4>
+              <p><b><?php echo $total_posts; ?></b></p>
+            </div>
+          </div>
+        </div>
+     </div>
+
+
+<?php require_once('includes/footer.php');
+
+?>
