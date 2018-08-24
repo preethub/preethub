52e7cdc5629fa1513d95e2dd8f135130b0ca5888
preethub <42606094+preethub@users.noreply.github.com>
2018-08-24 15:42:11 +0530

    Update ph-load.php

--- ph-load.php
+++ ph-load.php
@@ -5,17 +5,14 @@
 // load file - Version 0.1
 
 
-require('ph-preet/preet.php');
-
 // check config file
 if(file_exists('ph-config.php')){
 require ('ph-config.php');
 }else{
-	header('Location: /ph-install.php');
+	header('Location: ph-install.php');
 }
 
-
-	
+require('ph-preet/preet.php');
 
 
 
