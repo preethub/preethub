20b265392b5d85bc1fed653140b04b6fff9111d3
preethub <42606094+preethub@users.noreply.github.com>
2018-08-24 22:15:07 +0530

    Update inc.php

--- ph-admin/inc.php
+++ ph-admin/inc.php
@@ -1,25 +1,17 @@
 <?php
-
+/* -----------------@-----------------------------
+*Preethub
+------------------------------------------------*/
 session_start();
 
-$ph = new stdClass();
 
-require_once(dirname(dirname(__FILE__)) . '/ph-config.php');
+require_once(dirname(dirname(__FILE__)) . '/ph-preet/preet.php');
 
+$login = new login;
 
-require_once(dirname(dirname(__FILE__)) . '/ph-preet/includes/db.class.php');
-
-// db connection
-$db = new db($db_host,$db_user,$db_pass,$db_name);
-
-require_once(dirname(dirname(__FILE__)) . '/ph-preet/system/settings.php');
-
-	$usersession = $_SESSION['username'];
-	
-	$userquery = "SELECT * FROM ". PH_PRIFIX ."users WHERE username='$usersession'"; 
-	
-	$user = $db->get_row($userquery);
+$user = $login->userdata();
 	
 if($user->role != Admin){
 	header("location: $site_url");
 }
+
