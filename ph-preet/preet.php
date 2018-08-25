575593f6f961b017548b1fbf66b84488508b0c68
preethub <42606094+preethub@users.noreply.github.com>
2018-08-25 17:11:21 +0530

    Update preet.php

--- ph-preet/preet.php
+++ ph-preet/preet.php
@@ -2,16 +2,14 @@
 
 // define ROOTPATH
 if ( !defined('ROOTPATH') )
-	define('ROOTPATH', dirname(__FILE__) . '/');
+define('ROOTPATH', dirname(__FILE__) . '/');
 	
-		// define ABSPATH
-	if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(dirname(__FILE__)) . '/');
+// define ABSPATH
+if ( !defined('ABSPATH') )
+define('ABSPATH', dirname(dirname(__FILE__)) . '/');
 
 
 require('system/loader.php');
-
-require ABSPATH .'/ph-config.php';
 
 // db connection
 $db = new db(DB_HOST,DB_USER,DB_PASS,DB_NAME);
