ab4a4ea273e34381aa91aba2da0f9ff326edda1c
preethub <42606094+preethub@users.noreply.github.com>
2018-08-25 19:57:28 +0530

    Update post.php

--- ph-preet/view/post.php
+++ ph-preet/view/post.php
@@ -4,20 +4,19 @@
 
 <div class="row">
   <div class="leftcolumn">
-   
-        <?php foreach ($posts as $post): ?>
+  
          <div class="card">
       <h2><?php echo $post->name ?></h2>
       <h5>Published - <?php echo $post->created_at ?></h5> 
     <?php if(!empty($post->image)){ ?>
     <img src="<?php echo $site_url . '/ph-uploads/' . $post->image; ?>" style="width:350px;height:300px;">
-    <?php } echo $post->content; ?>  </div>
-    <?php endforeach ?>
+    <?php } echo $post->content; ?>  </div>    
   </div>
   <div class="rightcolumn">
     <div class="card">
-  <?php  if(empty($_SESSION['username'])){ ?>
+  <?php  if(empty($_SESSION['username'])){ ?>    
       <h3>Login</h3>
+     <h5> <?php echo $login->dispalyerrors(); ?></h5>
   <form action="" method="post">
     <label for="fname">User Name</label>
     <input type="text" id="fname" name="username" placeholder="Your name..">
@@ -30,7 +29,7 @@
   <?php }else{?>
 <h5> - <a href="/logout">Logout</a> <h5>
   <?php $user = $login->userdata();
-  if($user->role === Admin){ ?>  	
+  if($user->role === 'Admin'){ ?>  	
  - <a href="/ph-admin">Admin Panel</a>
   <?php } 
    } ?>
