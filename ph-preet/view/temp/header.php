7b865bb6ace8b680f03a6b69a3cf0c477942965b
preethub <42606094+preethub@users.noreply.github.com>
2018-08-25 19:28:03 +0530

    Update header.php

--- ph-preet/view/temp/header.php
+++ ph-preet/view/temp/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 <head>
+<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php echo $title; ?></title>
 <?php addcss('style.css'); ?>
 </head>
 <body>
