873a9e80aa549f7cca39a49621331140a1f89153
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2018-08-27 21:30:52 +0530

    Update post.php

--- ph-preet/view/post.php
+++ ph-preet/view/post.php
@@ -4,37 +4,40 @@
 
 <div class="row">
   <div class="leftcolumn">
-  
+        
          <div class="card">
-      <h2><?php echo $post->name ?></h2>
+<h2><?php echo $post->name ?></h2>
       <h5>Published - <?php echo $post->created_at ?></h5> 
-    <?php if(!empty($post->image)){ ?>
-    <img src="<?php echo $site_url . '/ph-uploads/' . $post->image; ?>" style="width:350px;height:300px;">
-    <?php } echo $post->content; ?>  </div>    
+<?php if(!empty($post->image)){ ?>
+  <img src="<?php echo $site_url . '/ph-uploads/' . $post->image; ?>" style="width:350px;height:300px;">
+    <?php } echo $post->content; ?>  </div>
   </div>
   <div class="rightcolumn">
+  <div class="heading">Comments</div>
+ <?php  if($comment->have_comments()){ 
+   foreach ($comments as $comment): ?>  
+     <div class="card">
+ <?php  $cuser = $db->get_row("SELECT * FROM `". PH_PREFIX ."users` WHERE id='$comment->comment_user_id'"); ?>   
+  <div class="imge"><img src="<?php echo $site_url ?>/ph-preet/view/style/images/default-avatar.png"style="width:35px;height:30px;"></div>
+<b><?php  echo  $cuser->username; ?></b> <br><?php echo $comment->created_at; ?>  <br><br>
+       <?php echo $comment->comment_content; ?>  
+      </div>
+   <?php endforeach;   }else{  ?>     
     <div class="card">
-  <?php  if(empty($_SESSION['username'])){ ?>    
-      <h3>Login</h3>
-     <h5> <?php echo $login->dispalyerrors(); ?></h5>
+    No Comments.
+    </div>
+    <?php } ?>     
+     <div class="card"> 
+  <?php  if(is_logged()){     ?>            
   <form action="" method="post">
-    <label for="fname">User Name</label>
-    <input type="text" id="fname" name="username" placeholder="Your name..">
-
-    <label for="lname">Password</label>
-    <input type="text" id="lname" name="password" placeholder="Your last name..">
-  
-    <input type="submit" value="Login" name="login">
+<b>Comment</b><br/><br>   
+            <textarea name="comment" rows="4" placeholder="comment"></textarea>
+    <input type="submit" value="Comment" name="addcomment">
   </form> 
-  <?php }else{?>
-<h5> - <a href="/logout">Logout</a> <h5>
-  <?php $user = $login->userdata();
-  if($user->role === 'Admin'){ ?>  	
- - <a href="/ph-admin">Admin Panel</a>
-  <?php } 
-   } ?>
-</div>
+   <?php }else{ ?>
+    You must <a href="<?php echo $site_url; ?>/login"><b>login</b></a> or <a href="<?php echo $site_url; ?>/signup"><b>signup</b></a> for post a comment.
+  <?php } ?>
+  </div>  
   </div>
 </div>
-
 <?php require('temp/footer.php'); ?>
