ee1c9d8a66ec2be7b8ce1961ba9244686cb8caef
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2018-08-28 11:58:20 +0530

    $total_comments

--- ph-admin/functions/index.php
+++ ph-admin/functions/index.php
@@ -4,3 +4,5 @@ $total_users = $db->count("SELECT `id` FROM `". PH_PREFIX ."users`");
 
 $total_posts = $db->count("SELECT `id` FROM `". PH_PREFIX ."posts`");
 
+$total_comments = $db->count("SELECT `comment_id` FROM `". PH_PREFIX ."comments`");
+
