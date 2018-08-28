afc82c3fc43f7d407bead62794741b464102833c
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2018-08-28 12:02:05 +0530

    Update index.php

--- ph-admin/index.php
+++ ph-admin/index.php
@@ -31,7 +31,7 @@ require('includes/header.php');
          <div class="col-md-6 col-lg-3">
           <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
             <div class="info">
-              <h4comments</h4>
+              <h4>comments</h4>
               <p><b><?php echo $total_comments; ?></b></p>
             </div>
           </div>
