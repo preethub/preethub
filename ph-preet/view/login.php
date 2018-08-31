666619a1be3dfd10d4b8e712442a13d5e4e9286e
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2018-08-31 15:44:57 +0530

    Update login.php

--- ph-preet/view/login.php
+++ ph-preet/view/login.php
@@ -2,15 +2,13 @@
  <?php require 'temp/header.php'; ?>
  <div class="card">
 <h3>Login</h3>
-<?php echo $login->displayerrors(); 
+<?php echo $ph_class->displayerrors(); 
 ?>
   <form action="" method="post">
     <label for="fname">User Name</label>
     <input type="text" id="fname" name="username" placeholder="Your name..">
-
     <label for="lname">Password</label>
     <input type="text" id="lname" name="password" placeholder="Your last name..">
-  
     <input type="submit" value="Login" name="login">
   </form>
 </div>
