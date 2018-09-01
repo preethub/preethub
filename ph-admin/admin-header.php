1b06747950fbda82b6c1dbe80a0aa0256a0c4855
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2018-09-01 21:48:30 +0530

    Create admin-header.php

---
+++ ph-admin/admin-header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+  <head>
+    <title><?php echo $site_name ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="<?php echo $site_url; ?>/ph-admin/style/admin.css">
+    <script>(function(e,t,n){var r=e.querySelectorAll("html")[0];r.className=r.className.replace(/(^|\s)no-js(\s|$)/,"$1js$2")})(document,window,0);</script> 
+  </head>
+ <body>
+ <header><a href="<?php echo $site_url; ?>"><?php echo $site_name; ?></a></header>	
+<div class="nav">				<ul><li><a href="index.php">Home</a></li>
+ <li><a href="users.php">All Users</a></li><li><a href="add-user.php">Add users</a></li><li><a href="posts.php">All Posts</a></li><li><a href="add-post.php">Add Posts</a></li><li><a href="settings.php">Settings</a></li>
+</ul>
+</div>
