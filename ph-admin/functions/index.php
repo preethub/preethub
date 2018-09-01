ac2af599a6e0413dcd140b1dac86b26e73668d61
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2018-09-01 21:29:21 +0530

    Delete index.php

--- ph-admin/functions/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$total_users = $db->count("SELECT `id` FROM `". PH_PREFIX ."users`");
-
-$total_posts = $db->count("SELECT `id` FROM `". PH_PREFIX ."posts`");
-
-$total_comments = $db->count("SELECT `comment_id` FROM `". PH_PREFIX ."comments`");
-
