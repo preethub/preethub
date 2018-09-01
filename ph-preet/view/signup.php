d1b751910ea3c580716597b23ad06f3469093e11
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2018-09-01 20:53:20 +0530

    Update signup.php

--- ph-preet/view/signup.php
+++ ph-preet/view/signup.php
@@ -1,24 +1,16 @@
 <?php require('temp/header.php'); ?>
-
  <div class="card">
-
 <h3> register </h3>
-<?php  echo $signup->displayerrors(); 
-?>
+<?php  echo $ph_class->displayerrors(); ?>
   <form action="signup" method="post">
     <label for="phuname">User Name</label>
     <input type="text" id="phuname" name="username" placeholder="Your username..">
-
 <label for="phemail">Email</label>
     <input type="text" id="phemail" name="email" placeholder="Your email..">
-
     <label for="phpass">Password</label>
-    <input type="text" id="phpass" name="password" placeholder="Your password..">
-  
+    <input type="text" id="phpass" name="password" placeholder="Your password.."> 
     <input type="submit" value="signup" name="signup">
   </form> </div>
-<?php require('temp/footer.php');
-?>
-
+<?php require('temp/footer.php'); ?>
 </body>
 </html>
