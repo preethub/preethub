be537fddbf883a165562034362928e573e7bbe5f
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2018-09-02 12:19:08 +0530

    Update header.php

--- ph-preet/view/temp/header.php
+++ ph-preet/view/temp/header.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo $title; ?></title>
-<?php addcss('style.css'); ?>
+<?php stylesheet('style.css'); ?>
 </head>
 <body>
 <div class="header">
