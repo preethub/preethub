b0e39132b1d3aed73304bfd72dff623378aebf4b
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2019-04-01 22:58:11 +0530

    Update general-functions.php

--- ph-preet/general-functions.php
+++ ph-preet/general-functions.php
@@ -1,115 +1,117 @@
 <?php
 
-define("dir", realpath("."));
-
-function filterUrl($str){
-  return htmlspecialchars(trim($str));
-}
-
-function get($name){
-  if(isset($_GET[$name])){
-
-    if (is_array($_GET[$name])){
-      return array_map(function($item){
-        return filterUrl($item);
-      }, $_GET[$name]);
-    }
-    return filterUrl($_GET[$name]);
-
-  }
-  return false;
-}
-
-function get_url($name){
-
-return dir. '/ph-preet/view/' .$name. '.php';
-
-}
+ /*---------------
+ * General functions
+ * Preethub
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ * github.com/preethub/preethub
+ *---------------*/
 
 
-function stylesheet($css){
+/*--- FUNCTION PREET ---*/
+function preet(){
+	global $getparams; 
+if(!empty($_GET)){	
+	$uri = explode("?",$_SERVER['REQUEST_URI']);
+$firstgetname = explode("=",$uri[1]);	
+		if(!in_array($firstgetname[0],$getparams)){			
+header("location:" . get_config('site_url'));
+		} 
+		
+if(isset($_GET['p'])){		
+	$id = (int) $_GET['p']; 	
+	page_title(get_page()->page_name ." - ". get_config('site_name'));	
+	default_page_view();	
+	}
+		
+/*--- RUN HOOK ISSET_GETPARAM---*/
+run_hook('isset_getparam');
 	
- global $site_url;	
- 
- echo '<link rel="stylesheet" type="text/css" href="' .$site_url. '/ph-preet/view/style/css/' .$css .'">';		
+}else{	
+	$id = get_config('site_index');
+	page_title(get_index()->page_name ." - ". get_config('site_name'));	
+ 	default_index_view();
+	}
 }
 
-
+/*--- FUNCTION IS_LOGGED ---*/
 function is_logged(){
 if(!empty($_SESSION['username'])){
  return TRUE;
- return FALSE;
- }
+ }else{return FALSE;
+    }
  }
  
- function page_title(){
- global $page_title;
- 
- return $page_title;
+ /*--- FUNCTION PAGE_TITLE ---*/
+function page_title($title = ''){
+global $page_title;
+if($title){
+ 	$page_title = $title;
+}else{
+ return $page_title; 
+ }
 }
 
+/*---  FUNCTION GET_MESSAGES ---*/
+function get_messages(){
+if(!empty($_SESSION['messageinfo'])){ $message = $_SESSION['messageinfo'];   
+unset($_SESSION['messageinfo']); 
+  return $message;  }           
+ }
 
-function site_info($name){
-
-global $db;
-
-$result = $db->get_row("SELECT * FROM `". PH_PREFIX ."config` Where name='$name'");
-
-return $result->value;
-
-}		
-
-function pagination($baseUrl, $totalResults, $resultsPerPage, $currentPage) {
-    $totalPages = ceil($totalResults/$resultsPerPage); 
-    if($totalPages <=1 )
-    return '';
-    $queryString = ''; 
-  $rightLinks = $currentPage+3;
- $previousLinks = $currentPage-3;
-    ob_start();    
-    ?>
-    <div class="card">
-    <ul class="pagination">       
-            <?php                  if($currentPage == 1) {     ?>            
-  <?php 
-    } if($currentPage > 1) {   ?>           <li><a href="<?php echo $baseUrl.'/page/'.($currentPage-1); ?>">Previous</a></li>
-            <?php 
-        }
-        for($i = $previousLinks; $i <= $currentPage; $i++){
-            if($i>0) {                if($i==$currentPage) { ?>
-                    <li class="current"><a href=""><?php echo $i; ?></a></li>
-            <?php } else { ?>
-       <li>
-      <a href="<?php echo $baseUrl.'/page/'.$i; ?>"><?php echo $i; ?></a>
-                    </li>
-                <?php }    
-            }            
-        }
-        if(false)
-        for($i=1; $i<=$totalPages; $i++) {
-            if($i==$currentPage) { ?>
-      <li class="active"><a href=""><?php echo $i; ?></a></li>
-            <?php }   else { ?>
-                <li>
-          <a href="<?php echo $baseUrl.'/page/'.$i; ?>"><?php echo $i; ?></a>
-                </li>
-        <?php }        }
-        for($i = $currentPage+1; $i < $rightLinks ; $i++){
-            if($i<=$totalPages){               if($i==$currentPage) { ?>
-                    <li class="active"><a href=""><?php echo $i; ?></a></li>
-       <?php }  else { ?>   <li><a href="<?php echo $baseUrl.'/page/'.$i; ?>"><?php echo $i; ?></a>
-                    </li>
-                    <?php
-                }
-            }
-        }
-        if($currentPage != $totalPages) { ?>
-             <li><a href="<?php echo $baseUrl.'/page/'.($currentPage+1); ?>">Next</a></li>
-       <?php   }  ?>
-    </ul>
-    </div>
-    <?php
-    $output = ob_get_contents();
-    ob_end_clean();
-    return $output;
+/*---  FUNCTION ADD_MESSAGES ---*/
+function add_message($msg){
+$_SESSION['messageinfo'] = $msg;
 }
+
+/*--- FUNCTION RUN_HOOK ---*/
+	function run_hook($hook,$value = ''){
+		 global $ph_hooks;
+if(isset($ph_hooks[$hook]))		
+{
+ foreach($ph_hooks[$hook] as $function)
+   { 
+    if(is_callable($function)){
+      $value = call_user_func($function, $value);
+   }
+ }
+   return $value;
+}else{
+  return $value;
+ }
+}
+
+/*--- FUNCTION ADD_HOOK ---*/
+function add_hook($hook,$function){
+ global $ph_hooks;
+ $ph_hooks[$hook][] = $function;
+ return true;
+}
+
+/*--- FUNCTION PAGINATION ---*/
+function pagination($total, $page, $perpage = 10){
+		
+		$total_pages = ceil($total/$perpage);
+		foreach($_GET as $k=>$v)
+			if($k != 'page')
+				$query .= "$k=$v&";
+		$pages = '<ul class="pagination">';
+		if($page > 4)
+			$pages .= "<a href='?$query'>First</a> ";
+		
+		if($page > 1)
+			$pages .= '<li><a href="?'. $query .'page='. ($page-1) .'">Prev</a></li> ';
+			
+		for($i = max(1, $page - 3); $i <= min($page + 3, $total_pages); $i++)
+			$pages .= ($i == $page ? '<li class="current">'. $i .'</li>' : ' <li><a href="?'. $query .'page='. $i .'">'. $i .'</a></li> ');
+
+		if($page < $total_pages)
+			$pages .= '<li><a href="?'. $query .'page='.($page+1).'">Next</a></li>';
+		
+		if($page < $total_pages-3)
+			$pages .= '<li><a href="?'. $query .'page='. $total_pages .'"> Last </a></li>';
+		$pages .= '</ul>';
+		return $pages;
+	
+}	
