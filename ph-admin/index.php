575ec938aa7b8a78efcd454d6bec3dc73feaeb85
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2019-04-01 23:31:23 +0530

    Update index.php

--- ph-admin/index.php
+++ ph-admin/index.php
@@ -1,13 +1,14 @@
-<?php 
-require('admin-core.php');
-require('admin-header.php');
-?>    
-      <div class="heading">Dashboard</div>
-      <div class="card">
-         <p>  Total Users        -      <b><?php echo $total_users; ?></b></p>
-         <p>   Total Posts   -              <b><?php echo $total_posts; ?></b></p>
-     <p>Total Comments   -              <b><?php echo $total_comments; ?></b></p>           
-   </div>             
-<?php require('admin-footer.php');
+<?php require('admin-header.php');  ?>
 
-?>
+      <div class="title">	<i class="fa fa-tachometer"></i> Dashboard</div>
+      <div class="widget">
+      <div class="title">Site Info</div>
+     <div class="card">
+         <p>  Total Users        -      <b><?php echo total_users() ?></b></p>
+
+  <p> Total Pages   -              <b><?php echo total_pages() ?></b></p>     
+<?php run_hook('admin_siteinfo') ?>      
+   </div> 
+   </div>         
+
+<?php  require('admin-footer.php'); ?>
