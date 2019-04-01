cb4d9265a3f4d1c39446d63aab349a3aeed62f50
Gurpreethundal <42700015+Gurpreethundal@users.noreply.github.com>
2019-04-01 23:26:04 +0530

    Update admin-footer.php

--- ph-admin/admin-footer.php
+++ ph-admin/admin-footer.php
@@ -1,2 +1,5 @@
-<script src="<?php echo $site_url ?>/ph-admin/style/js/main.js"></script>
-<footer><?php echo $site_name; ?> - <?php echo $ph_version; ?></footer>
+ <?php run_hook('admin_foot'); ?>
+ <script src="js/jquery-3.3.1.min.js"></script>	
+<script src="js/admin.js"></script>	
+
+<footer class="footer"><?php echo get_config('site_name'); ?> - <?php echo $ph_version; ?></footer>
