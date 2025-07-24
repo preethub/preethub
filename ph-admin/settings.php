85d2a99785d069928cb6c6c685651255b07c6f47
preethub <42606094+preethub@users.noreply.github.com>
2025-07-25 01:10:31 +0530

    Update settings.php

--- ph-admin/settings.php
+++ ph-admin/settings.php
@@ -1,23 +1,55 @@
 <?php 
 require('admin-header.php');
 settings();
-?>    
-     <div class="title">
-         Settings
-        </div> 
-         <div class="widget"> 
+?>
+
 <div class="title">Settings</div>
-            <div class="card">      <?php echo get_messages(); ?>     
-            <form action="settings.php" method="post">                   
-                  <label for="site-url"><b>Site Url </b></label>
-       <input name="siteurl" type="text" value="<?php echo get_config('site_url'); ?>" required>
-         <a href="<?php echo get_config('site_url'); ?>"><b><?php echo get_config('site_url'); ?></b></a>  <br>  <br>                                                
-                  <label for="site-name"><b>Site Name</b></label>
-                  <input name="sitename" type="text" value="<?php echo get_config('site_name'); ?>" required>
-                  <label for="site-description"><b>Site Description </b></label>
-              <textarea name="sitedesc" rows="4"><?php echo get_config('site_description'); ?></textarea>
-                  <input type="submit" name="settings">                
-              </form>
-            </div>
-            </div>
+
+<div class="widget"> 
+    <div class="title">Settings</div>
+    <div class="card">      
+        <?php echo get_messages(); ?>     
+
+        <form action="settings.php" method="post">
+
+            <label for="site-url"><b>Site URL</b></label>
+            <input 
+                id="site-url" 
+                name="siteurl" 
+                type="text" 
+                value="<?php echo htmlspecialchars(get_config('site_url'), ENT_QUOTES, 'UTF-8'); ?>" 
+                required
+            >
+            <a href="<?php echo htmlspecialchars(get_config('site_url'), ENT_QUOTES, 'UTF-8'); ?>">
+                <b><?php echo htmlspecialchars(get_config('site_url'), ENT_QUOTES, 'UTF-8'); ?></b>
+            </a>
+
+            <br><br>
+
+            <label for="site-name"><b>Site Name</b></label>
+            <input 
+                id="site-name" 
+                name="sitename" 
+                type="text" 
+                value="<?php echo htmlspecialchars(get_config('site_name'), ENT_QUOTES, 'UTF-8'); ?>" 
+                required
+            >
+
+            <label for="site-description"><b>Site Description</b></label>
+            <textarea 
+                id="site-description" 
+                name="sitedesc" 
+                rows="4"
+            ><?php echo htmlspecialchars(get_config('site_description'), ENT_QUOTES, 'UTF-8'); ?></textarea>
+
+            <input 
+                type="submit" 
+                name="settings" 
+                value="Save Settings"
+            >
+
+        </form>
+    </div>
+</div>
+
 <?php require('admin-footer.php'); ?>
