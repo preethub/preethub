00e58908a512052527266d4c1f69556d0956f542
preethub <42606094+preethub@users.noreply.github.com>
2025-07-25 00:55:25 +0530

    Update manage.php

--- ph-admin/manage.php
+++ ph-admin/manage.php
@@ -3,80 +3,91 @@ require('admin-header.php');
 
 run_hook('admin_manage_head');
 
-if(isset($_GET['type'])){
+// Get the request type and ID for deletion, if available
+$type = $_GET['type'] ?? null;
+$deleteId = $_GET['delete'] ?? null;
 
-if($_GET['type'] === 'page'){  
+if ($type) {
+    echo '<div class="title"><i class="fa fa-tachometer"></i> All ' . ucfirst($type) . 's</div>';
+    echo '<div class="widget"><div class="card">';
+    echo get_messages();
 
-delete_page(); ?>
-   
-  	<div class="title">	<i class="fa fa-tachometer"></i> All Pages</div> 
-     <div class="widget">
-   <div class="card">   
-    <?php echo get_messages(); ?>
-                   <table>
-                    <thead>
-                      <tr>        
-                  <th>Id</th>        
-                  <th>Name</th>   
-                  <th>Action</tr>
-                      </tr>
-                    </thead>
-                    <tbody>
- <?php
- if(total_pages() > 0){
-  foreach (get_pages() as $page): ?>
- <tr>
-<td> <?php echo $page->page_id; ?></td>
-<td> <?php echo $page->page_name; ?> </td>
-<td> <?php echo $page->page_name; ?> </td>
-<td> <a href="<?php echo get_config('site_url'); ?>/?p=<?php echo $page->page_id ?>">View</a> - <a href="edit.php?type=page&id=<?php echo $page->page_id ?>">Edit</a> - <?php if(get_config('site_index') ===  $page->page_id){ ?> <a onclick= "alert('Index page can not be deleted')"><b>Delete</b></a> <?php }else{ ?> <a href="?type=page&delete=<?php echo $page->page_id; ?>" onclick= "if(! confirm('Are you sure?')){return false;}"><b>Delete</b></a>  <?php } ?></td>
-</tr>
- <?php endforeach; } ?>                                                                       
-                 </tbody>
-                  </table>   
-                </div>               
-                </div>
- 
- <?php     
-}elseif($_GET['type'] === 'user'){
+    if ($type === 'page') {
+        if ($deleteId) {
+            delete_page((int)$deleteId);
+        }
 
-delete_user();
-?>
-     <div class="title">	<i class="fa fa-tachometer"></i> All Users</div> 
-      <div class="widget">
-     <div class="card"> 
-    <?php echo get_messages(); ?>
-                   <table>
-                    <thead>
-                      <tr>        
-                  <th>Id</th>        
-                  <th>Name</th>   
-                  <th>Role </th>       
-                  <th>Action</tr>
-                      </tr>
-                    </thead>
-                    <tbody>
-   <?php 
-   if(total_users() > 0){
-   foreach (get_users() as $udata): ?><tr>
-<td><?php echo $udata->user_id ?></td>  
-<td> <?php echo $udata->username ?></td>        
-                   <td> <?php	echo $udata->role;   ?>
-               </td> <td> <?php if(get_config('site_admin') ===  $udata->username){ ?> <div class="redtext">Super Administrator</div> <?php }else{ ?> <a href="?type=user&delete=<?php echo $udata->user_id; ?>" onclick= "if(! confirm('Are you sure?')){return false;}"><b>Delete</a></b> <?php } ?></td>
-               </tr>    
-            <?php endforeach; }?>                                                              
-                 </tbody>
-                  </table> 
-                    </div>                
-                </div>
- 	
- 	    
+        echo '<table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>';
 
-<?php
- 
-  	} 
-}		
+        if (total_pages() > 0) {
+            foreach (get_pages() as $page) {
+                echo '<tr>
+                        <td>' . htmlspecialchars($page->page_id) . '</td>
+                        <td>' . htmlspecialchars($page->page_name) . '</td>
+                        <td>
+                            <a href="' . get_config('site_url') . '/?p=' . $page->page_id . '">View</a> -
+                            <a href="edit.php?type=page&id=' . $page->page_id . '">Edit</a> - ';
+
+                if (get_config('site_index') === $page->page_id) {
+                    echo '<span class="redtext"><b>Cannot Delete Index</b></span>';
+                } else {
+                    echo '<a href="?type=page&delete=' . $page->page_id . '" onclick="return confirm(\'Are you sure?\')"><b>Delete</b></a>';
+                }
+
+                echo '</td>
+                    </tr>';
+            }
+        }
+
+        echo '</tbody></table>';
+
+    } elseif ($type === 'user') {
+        if ($deleteId) {
+            delete_user((int)$deleteId);
+        }
+
+        echo '<table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Role</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>';
+
+        if (total_users() > 0) {
+            foreach (get_users() as $user) {
+                echo '<tr>
+                        <td>' . htmlspecialchars($user->user_id) . '</td>
+                        <td>' . htmlspecialchars($user->username) . '</td>
+                        <td>' . htmlspecialchars($user->role) . '</td>
+                        <td>';
+
+                if (get_config('site_admin') === $user->username) {
+                    echo '<div class="redtext">Super Administrator</div>';
+                } else {
+                    echo '<a href="?type=user&delete=' . $user->user_id . '" onclick="return confirm(\'Are you sure?\')"><b>Delete</b></a>';
+                }
+
+                echo '</td></tr>';
+            }
+        }
+
+        echo '</tbody></table>';
+    }
+
+    echo '</div></div>';
+}
 
 run_hook('admin_manage_foot');
-
-require('admin-footer.php');            
+require('admin-footer.php');
