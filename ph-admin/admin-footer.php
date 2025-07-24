d27fbc9d6e7d50c3375759fabd6b275fb9edb010
preethub <42606094+preethub@users.noreply.github.com>
2025-07-25 01:01:30 +0530

    Update admin-footer.php

--- ph-admin/admin-footer.php
+++ ph-admin/admin-footer.php
@@ -1,5 +1,11 @@
- <?php run_hook('admin_foot'); ?>
- <script src="js/jquery-3.3.1.min.js"></script>	
-<script src="js/admin.js"></script>	
+<?php run_hook('admin_foot'); ?>
 
-<footer class="footer"><?php echo get_config('site_name'); ?> - <?php echo $ph_version; ?></footer>
+<!-- JavaScript Files -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/admin.js"></script>
+
+<!-- Footer -->
+<footer class="footer">
+    <?php echo htmlspecialchars(get_config('site_name')); ?> - 
+    <?php echo htmlspecialchars($ph_version); ?>
+</footer>
